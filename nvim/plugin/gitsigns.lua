047b4d4ca73b17e05b2b4e482a587d96f07fd8fb
fans3210 <liyaodong2@gmail.com>
2021-10-31 13:10:27 +0800

    overwrite gitsigns default keymaps

--- nvim/plugin/gitsigns.lua
+++ nvim/plugin/gitsigns.lua
@@ -1,1 +1,7 @@
-require 'gitsigns'.setup()
+require'gitsigns'.setup {
+    keymaps = {
+        noremap = true,
+        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>'
+    }
+}
