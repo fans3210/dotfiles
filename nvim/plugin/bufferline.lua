6837d29d3c1f71be8b0b1a0b7c514ea9a81edd6a
fans3210 <liyaodong2@gmail.com>
2021-11-01 14:19:58 +0000

    commit

--- nvim/plugin/bufferline.lua
+++ nvim/plugin/bufferline.lua
@@ -1,4 +1,4 @@
-require("bufferline").setup {}
+require("bufferline").setup {options = {numbers = 'ordinal'}}
 
 vim.cmd [[
 nnoremap <silent><TAB> :BufferLineCycleNext<CR>
@@ -13,4 +13,8 @@ nnoremap <silent><leader>7 <Cmd>BufferLineGoToBuffer 7<CR>
 nnoremap <silent><leader>8 <Cmd>BufferLineGoToBuffer 8<CR>
 nnoremap <silent><leader>9 <Cmd>BufferLineGoToBuffer 9<CR>
 
+
+nnoremap <silent><leader>xl <Cmd>BufferLineCloseLeft<CR>
+nnoremap <silent><leader>xr <Cmd>BufferLineCloseRight<CR>
+nnoremap <silent><leader>xp <Cmd>BufferLinePickClose<CR>
 ]]
