26d535b63a693e96129d2da6211a5fe746e6074f
fans3210 <liyaodong2@gmail.com>
2021-11-29 20:41:13 +0800

    commit

--- nvim/plugin/tree-sitter.lua
+++ nvim/plugin/tree-sitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     highlight = {enable = true, disable = {}},
     indent = {enable = false, disable = {}},
-    ensure_installed = {"tsx", "json", "yaml", "html", "scss", "python", "javascript", "typescript"},
+    ensure_installed = {"tsx", "json", "yaml", "html", "scss", "python", "javascript", "typescript", "go"},
     context_commentstring = {enable = true}
 }
 local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
