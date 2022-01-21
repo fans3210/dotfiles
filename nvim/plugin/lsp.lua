88abdfc59b57797ef494cf6f396014fed3603779
fans3210 <liyaodong2@gmail.com>
2022-01-21 22:09:08 +0800

    begin to meerge main

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -24,7 +24,11 @@ end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- TypeScript
-nvim_lsp.tsserver.setup {on_attach = on_attach, filetypes = {"typescript", "typescriptreact", "typescript.tsx"}, capabilities = capabilities}
+nvim_lsp.tsserver.setup {
+    on_attach = on_attach,
+    filetypes = {"javascript", "typescript", "typescriptreact", "typescript.tsx"},
+    capabilities = capabilities
+}
 
 nvim_lsp.diagnosticls.setup {
     on_attach = on_attach,
