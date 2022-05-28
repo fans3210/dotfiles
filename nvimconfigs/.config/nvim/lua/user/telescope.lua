89759767bc8afb1729722fd71389c216bb91ebb6
fans3210 <liyaodong2@gmail.com>
2022-05-28 14:24:25 +0800

    commit

--- nvimconfigs/.config/nvim/lua/user/telescope.lua
+++ nvimconfigs/.config/nvim/lua/user/telescope.lua
@@ -8,9 +8,9 @@ require('telescope').setup {
                 height = 0.95,
             },
         },
-        file_ignore_patterns = {"node_modules", "package%-lock.json"},
+        file_ignore_patterns = { "node_modules", "package%-lock.json" },
         mappings = {
-            n = {["q"] = actions.close},
+            n = { ["q"] = actions.close },
             i = {
                 ["<C-c>"] = actions.close,
                 ["<C-q>"] = actions.send_to_qflist
@@ -19,18 +19,19 @@ require('telescope').setup {
                 -- ["<C-x>"] = actions.delete_buffer
             }
         },
-        extensions = {fzf = {override_generic_sort = false, override_file_sorter = true, case_mode = "smart_case"}}
+        extensions = { fzf = { override_generic_sort = false, override_file_sorter = true, case_mode = "smart_case" } }
     }
 }
 require('telescope').load_extension('fzf')
 
-vim.api.nvim_set_keymap('n', '<c-p>', [[<cmd>lua require'telescope.builtin'.find_files()<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', ';r', [[<cmd>lua require'telescope.builtin'.live_grep()<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<c-p>', [[<cmd>lua require'telescope.builtin'.find_files()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';r', [[<cmd>lua require'telescope.builtin'.live_grep()<CR>]], { noremap = true, silent = true })
 
 -- double slash(escaped)
-vim.api.nvim_set_keymap('n', '\\\\', [[<cmd>Telescope buffers<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '\\\\', [[<cmd>Telescope buffers<CR>]], { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', ';;', [[<cmd>Telescope help_tags<CR>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fc', [[<cmd>lua require'telescope.builtin'.quickfix()<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', ';;', [[<cmd>Telescope help_tags<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fc', [[<cmd>lua require'telescope.builtin'.quickfix()<CR>]], { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>vrc', [[<cmd>lua require'theprimeagen.telescope'.search_dotfiles()<CR>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>vrc', [[<cmd>lua require'theprimeagen.telescope'.search_dotfiles()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>hh', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", { noremap = true, silent = true })
